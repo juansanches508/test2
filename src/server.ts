791c0457847723c2d58c2bca83dd91cf41b89496
kObe <elkobexx@gmail.com>
2022-03-12 12:32:15 +0800

    main

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const app = App.app;
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
-    console.log("Server Online on port "+PORT);
+    console.log("Server Online on port  "+PORT);
 });
 
 /* =============== GITHUB ===============
